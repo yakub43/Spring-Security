7982c101c4447624a4f81538100349bb29e2a4d9
yakub43 <yakubakku43@gmail.com>
2024-04-09 20:30:57 +0530

    Modified congig file

--- security/src/main/java/com/security/eazybank/configuration/ProjectSecurityConfig.java
+++ security/src/main/java/com/security/eazybank/configuration/ProjectSecurityConfig.java
@@ -1,17 +1,11 @@
 package com.security.eazybank.configuration;
-
-import org.springframework.cglib.proxy.NoOp;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 
 @Configuration
 public class ProjectSecurityConfig {
 
+/*
     public InMemoryUserDetailsManager userDetailsService(){
         UserDetails admin = User.withUsername("admin")
                 .password("1234")
@@ -28,5 +22,5 @@ public class ProjectSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
-    }
+    }*/
 }
